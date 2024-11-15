25a175f8ea4926a440ad3c5280c9bedeb5d7ba87
Kristian Detlevsen <kbde63300@edu.ucl.dk>
2024-11-15 12:09:23 +0100

    feat(frontend): MainWindow-page

--- Robust/View/MainWindow.xaml.cs
+++ Robust/View/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Robust.ViewModel.ProductViewModel;
 
 namespace Robust.View.MainWindow;
 
@@ -19,5 +20,8 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        ProductViewModel pvm = new ProductViewModel();
+        DataContext = pvm;
     }
 }
--- Robust/ViewModel/ProductViewModel.cs
+++ Robust/ViewModel/ProductViewModel.cs
@@ -6,6 +6,8 @@ using Robust.Model.Product;
 using Robust.MVVM.ViewModelBase;
 using Robust.Repositories.Product;
 using System.Collections.ObjectModel;
+using Robust.Enums.Category;
+using System.Windows;
 
 namespace Robust.ViewModel.ProductViewModel;
 
@@ -13,6 +15,73 @@ internal class ProductViewModel : ViewModelBase
 {
     private ProductRepository _repository = new();
     private ObservableCollection<Product> _products = [];
+
+    //This Collection is bound to the combobox which displays all the different categories.
+    public ObservableCollection<Category> Categories { get; set; }
+    //This Collection is used for adding products to the MainWindow
+    public ObservableCollection<Product> Products { get; set; }
+    //This Collection contains the products that are to be displayed in the MainWindow.
+    public ObservableCollection<Product> SelectedProducts { get; set; }
+
+    //This property is used to select images based on their category.
+    private Category _selectedCategory;
+
+    public Category SelectedCategory
+    {
+        get { return _selectedCategory; }
+        set 
+        { 
+            _selectedCategory = value;
+            OnPropertyChanged();
+            DisplayProducts();
+            CheckDisplayAll();
+            CheckAvailability();            
+        }
+    }
+
+    //This method displays products from SelectedProducts which have the SelectedCategory.
+    private void DisplayProducts()
+    {
+        SelectedProducts.Clear();
+        
+        foreach (Product product in Products)
+        {
+            if (product.Category == SelectedCategory)
+            {
+                SelectedProducts.Add(product);
+            }
+        }
+    }
+
+    //This method checks if the category 'AlleKategorier' is selected.
+    private void CheckDisplayAll()
+    {
+        if (SelectedCategory == Category.AlleKategorier)
+        {
+            foreach (Product product in Products)
+            {
+                SelectedProducts.Add(product);
+            }
+        }
+    }
+
+    //This method shows a messagebox if there are no products in the chosen category.
+    private void CheckAvailability()
+    {
+        if (SelectedProducts.Count == 0)
+        {
+            MessageBox.Show("Vi kunne desværre ikke finde nogen produkter, som matcher din søgning. Prøv en anden kategori.");
+        }
+    }
+
+    //This method fills the collection SelectedProducts with all products from the collection Products.
+    private void FillSelectedProducts()
+    {
+        foreach (Product product in Products)
+        {
+            SelectedProducts.Add(product);
+        }
+    }   
 
     private void LoadProducts()
     {
@@ -39,5 +108,20 @@ internal class ProductViewModel : ViewModelBase
         throw new NotImplementedException();
     }
 
-    public ProductViewModel() { }
+    //The content of this constructor is mostly used for testing purposes at this moment.
+    public ProductViewModel()
+    { 
+        Categories = new ObservableCollection<Category>(Enum.GetValues(typeof(Category)).Cast<Category>());
+        
+        SelectedProducts = new ObservableCollection<Product>();
+        
+        Products = new ObservableCollection<Product>
+        {
+            new Product { Name = "Bustur", Category = Category.Transport, Description = "Et piktogram, der forestiller en bus", ImagePath = "C:/temp/Robust/Bus.jpg", Price = 38.00M },
+            new Product { Name = "Håndvask", Category = Category.Hygiejne, Description = "Et piktogram, som viser håndvask", ImagePath = "C:/temp/Robust/Handwashing.jpeg", Price = 38.00M },
+            new Product { Name = "Indkøbsvogn", Category = Category.IndkobOgShopping, Description = "Et piktogram, som viser en indkøbsvogn med varer", ImagePath = "C:/temp/Robust/ShoppingCart.jpg", Price = 38.00M }
+        };
+
+        FillSelectedProducts();
+    }
 }
