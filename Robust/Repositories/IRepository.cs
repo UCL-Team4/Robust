dc45d42ff95aa3037d00075699957049bf404c0d
Kristian Detlevsen <kbde63300@edu.ucl.dk>
2024-12-08 15:28:27 +0100

    feat: Add database connection

--- Robust/Repositories/IRepository.cs
+++ Robust/Repositories/IRepository.cs
@@ -1,8 +1,11 @@
-﻿namespace Robust.Repositories.Interface;
+﻿using System.Collections.ObjectModel;
+using Robust.Model.Product;
+
+namespace Robust.Repositories.Interface;
 
 public interface IRepository
 {
-    public void GetAll();
+    public ObservableCollection<Product> GetAll();
     public void GetByID();
     public void Add();
     public void Update();
